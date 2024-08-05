b8bfa1e989fde9d0e8169f492a1a27e39b38c695
SeeBeeSee <86379371+SeeBeeSee@users.noreply.github.com>
2024-08-04 19:15:02 -0500

    add simple navbar for pages; also jquery is cool

---
+++ src/nav.js
@@ -0,0 +1,55 @@
+// navigation bar functionality
+
+// Pages to populate the navbar with, and accompanying data
+// (update here instead of re-writing navbar for each page)
+const pages = [
+  {
+    title: "Seeb's Grocery List",
+    link: "grocery.html",
+  },
+  {
+    title: "Sample page",
+    link: "sample.html",
+  },
+];
+
+// When the page is ready, populate the navbar
+$(document).ready(function () {
+  let urlSplit = document.location.href.split("/");
+  let currentPage = urlSplit[urlSplit.length - 1];
+
+  // distinguish whether we're at top-level index or a sub-page (assuming only 1 level of depth)
+  if (currentPage === "index.html") {
+    // append home link
+    $(".navigation-bar").append(
+      "<button><a href='index.html'>Home</a></button>"
+    );
+
+    // append remaining pages
+    pages.forEach((pageData) => {
+      let newButton =
+        "<button><a href='./pages/" +
+        pageData.link +
+        "'>" +
+        pageData.title +
+        "</a></button>";
+      $(".navigation-bar").append(newButton);
+    });
+  } else {
+    // append home link
+    $(".navigation-bar").append(
+      "<button><a href='../index.html'>Home</a></button>"
+    );
+
+    // append remaining pages
+    pages.forEach((pageData) => {
+      let newButton =
+        "<button><a href='" +
+        pageData.link +
+        "'>" +
+        pageData.title +
+        "</a></button>";
+      $(".navigation-bar").append(newButton);
+    });
+  }
+});
